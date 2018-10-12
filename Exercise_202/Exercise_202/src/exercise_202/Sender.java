c1dccabaa3fab8490b2852e9c7da8790f598dae3
Kilian <kiliansc5@gmail.com>
2018-10-12 12:25:20 +0200

    Filled Sender with logic and ENUM

--- Exercise_202/Exercise_202/src/exercise_202/Sender.java
+++ Exercise_202/Exercise_202/src/exercise_202/Sender.java
@@ -7,4 +7,33 @@ package exercise_202;
  */
 public class Sender {
 
+    
+    private String name;
+    private double frequenz;
+    private Band band;
+    private enum Band{
+        AM,FM;
+    }
+
+    public Sender(String name, double frequenz, Band band) {
+        this.name = name;
+        this.frequenz = frequenz;
+        this.band = band;
+    }
+
+    public Band getBand() {
+        return band;
+    }
+
+    public double getFrequenz() {
+        return frequenz;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    
+    
+    
 }
